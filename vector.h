bae3078a59ffa404371f2095885bcf06984efad2
Lars-Dominik Braun <lars@6xq.net>
2015-02-14 15:05:45 +0100

    Support clang vector attribute

--- vector.h
+++ vector.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#if defined(__clang__)
+#if __has_extension(attribute_ext_vector_type)
+/* LLVM/clang */
+typedef double double2 __attribute__ ((ext_vector_type (2)));
+typedef double double4 __attribute__ ((ext_vector_type (4)));
+#endif
+#else
+/* GCC */
 typedef double double2 __attribute__ ((vector_size (sizeof (double)*2)));
 typedef double double4 __attribute__ ((vector_size (sizeof (double)*4)));
+#endif
 
