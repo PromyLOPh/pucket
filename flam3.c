PromyLOPh/pucket

commit b66dc836db66e9769f5c7142104c80fda6dd386b

Clean up apply_xform

flam3.c
@@ -175,7 +175,7 @@ bool iterator_step (iterator * const iter, double4 * const ret, randctx * const 
 	else
 		fn = iter->xform_distrib[ rand_u64(rc) & CHOOSE_XFORM_GRAIN_M1 ];
 
-	if (apply_xform(genome, fn, iter->p, &q, rc)>0) {
+	if (apply_xform(&genome->xform[fn], fn, iter->p, &q, rc)>0) {
 		++iter->consec;
 		if (iter->consec < 5) {
 			iter->p = q;
@@ -193,7 +193,8 @@ bool iterator_step (iterator * const iter, double4 * const ret, randctx * const 
 	if (genome->final_xform_enable == 1) {
 		if (genome->xform[genome->final_xform_index].opacity==1 || 
 				rand_d01(rc)<genome->xform[genome->final_xform_index].opacity) {
-			apply_xform(genome, genome->final_xform_index, iter->p, &q, rc);
+			apply_xform(&genome->xform[fn], genome->final_xform_index, iter->p,
+					&q, rc);
 			/* Keep the opacity from the original xform */
 			q = (double4) { q[0], q[1], q[2], iter->p[3] };
 		}

variations.h
@@ -22,6 +22,7 @@
 void xform_precalc (flam3_xform * const xform);
 int prepare_precalc_flags(flam3_genome *);
 
-int apply_xform(const flam3_genome * const cp, int fn, const double4 p, double4 *q_ret, randctx *rc);
+int apply_xform(const flam3_xform * const xf, const unsigned int fn,
+		const double4 p, double4 *q_ret, randctx * const rc);
 void initialize_xforms(flam3_genome *thiscp, int start_here);
 

commit 179075df17fd5c30d4a8a5984c1637ab67433248

Fix final transformation
Regression introduced by commit b66dc836db66e9769f5c7142104c80fda6dd386b

flam3.c
@@ -169,7 +169,7 @@ bool iterator_step (iterator * const iter, double4 * const ret, randctx * const 
 	else
 		fn = iter->xform_distrib[ rand_u64(rc) & CHOOSE_XFORM_GRAIN_M1 ];
 
-	if (apply_xform(&genome->xform[fn], fn, iter->p, &q, rc)>0) {
+	if (apply_xform(&genome->xform[fn], iter->p, &q, rc)>0) {
 		++iter->consec;
 		if (iter->consec < 5) {
 			iter->p = q;
@@ -187,7 +187,7 @@ bool iterator_step (iterator * const iter, double4 * const ret, randctx * const 
 	if (genome->final_xform_enable == 1) {
 		if (genome->xform[genome->final_xform_index].opacity==1 || 
 				rand_d01(rc)<genome->xform[genome->final_xform_index].opacity) {
-			apply_xform(&genome->xform[fn], genome->final_xform_index, iter->p,
+			apply_xform(&genome->xform[genome->final_xform_index], iter->p,
 					&q, rc);
 			/* Keep the opacity from the original xform */
 			q = (double4) { q[0], q[1], q[2], iter->p[3] };

variations.c
@@ -2030,8 +2030,8 @@ int prepare_precalc_flags(flam3_genome *cp) {
    return(0);
 }
 
-int apply_xform(const flam3_xform * const xf, const unsigned int fn,
-		const double4 p, double4 *q_ret, randctx * const rc) {
+int apply_xform(const flam3_xform * const xf, const double4 p, double4 *q_ret,
+		randctx * const rc) {
 	flam3_iter_helper f = { .rc = rc, .xform = xf };
 
 	const double s1 = xf->color_speed;

variations.h
@@ -22,6 +22,6 @@
 void xform_precalc (flam3_xform * const xform);
 int prepare_precalc_flags(flam3_genome *);
 
-int apply_xform(const flam3_xform * const xf, const unsigned int fn,
-		const double4 p, double4 *q_ret, randctx * const rc);
+int apply_xform(const flam3_xform * const xf, const double4 p, double4 *q_ret,
+		randctx * const rc);
 